b5765c95ba3487d80c6e17dc9ed54a3984afda15
Shivangkukreti <shivangkukretiwork@gmail.com>
2025-07-13 02:01:10 +0530

    yes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express= require('express')
 const app =express()
 const cors=require('cors')
 require('dotenv').config()
-
+const clerkweb =require('./config/webhooks.js')
 const  mongoose=require('mongoose')
 const uri=process.env.MONGO_URL
-
+const PORT=process.env.PORT || 5000
 main().then(()=>{
     console.log('done');
 }).catch((err)=>{
@@ -18,15 +18,15 @@ async function main() {
 
 }
 
-const clerkweb =require('./config/webhooks.js')
-
-const PORT=process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
 
 
-app.post("/webhooks", clerkweb);
+
+
+
+
 
 
 
@@ -43,6 +43,8 @@ app.listen(PORT,()=>{
 app.get('/',(req,res)=>{
     res.send('working')
 })
+
+app.post("/webhooks", clerkweb);
 
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
